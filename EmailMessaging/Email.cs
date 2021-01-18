1d89a65c96d55a29c26c7a74d4784092c77313f9
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2021-01-18 19:28:54 -0400

    Modificaciones en Host  y  Port

--- EmailMessaging/Email.cs
+++ EmailMessaging/Email.cs
@@ -6,6 +6,7 @@ namespace EmailMessaging
     using System.Net.Mail;
     using System.Threading.Tasks;
     using System.Text.RegularExpressions;
+    using System.Text;
 
     public static class Email
     {
@@ -15,7 +16,7 @@ namespace EmailMessaging
         /// <param name="emailCredential">credenciales del Email propietario.</param>
         /// <param name="To">Email de destino.</param>
         /// <param name="Subject">Asulto del mesaje.</param>
-        /// <param name="MessageBsody">Cuerpo del mensaje.</param>
+        /// <param name="MessageBody">Cuerpo del mensaje.</param>
         /// <param name="clientEmail">Cliente email</param>
         /// <param name="AttachDocuments">Documnetos adjuntos.</param>
         /// <returns>Retorn 
@@ -25,50 +26,61 @@ namespace EmailMessaging
         /// 4: Si ubo un error al embiar el mensaje.
         /// </returns>
         /// 
-        public static int SendMail
-        (EmailCredential emailCredential, string To, string Subject, string MessageBsody, ICollection<AttachDocuments> AttachDocuments = null)
+        public static result SendMail
+        (
+            EmailCredential emailCredential,
+            string To,
+            string Subject,
+            string MessageBody,
+            ICollection<Attachment> AttachmentsDocuments = null
+        )
         {
             if (!VerifyEmail(To))
-                return 2; //Email de destino incorrecto.
+                return result.WrongDestinationEmail; //Email de destino incorrecto.
 
             if (!VerifyEmail(emailCredential.Email))
-                return 3; //Email incorrecto.
+                return result.WrongEmail; //Email incorrecto.
 
             MailMessage Mail = new MailMessage();
+
             Mail.To.Add(new MailAddress(To));
             Mail.From = new MailAddress(emailCredential.Email);
+
             Mail.Subject = Subject;
-            Mail.Body = MessageBsody;
+            Mail.SubjectEncoding = Encoding.UTF8;
+
+
+            Mail.Body = MessageBody;
+            Mail.BodyEncoding = Encoding.UTF8;
+
             Mail.IsBodyHtml = true;
             Mail.Priority = MailPriority.High;
 
-            if (AttachDocuments.Equals(null))
-                Parallel.ForEach(AttachDocuments, Document =>
+            if (AttachmentsDocuments != null)
+                Parallel.ForEach(AttachmentsDocuments, Attachments =>
                 {
-                    Mail.Attachments.Add(new Attachment(Document.Document, Document.Name + Document.Extension));
+                    Mail.Attachments.Add(Attachments);
                 });
 
-            SmtpClient Smtp = new SmtpClient();
-            Smtp.Host = GetClientEmail(emailCredential.Email);
-            Smtp.Port = GetClientPost(emailCredential.Email);
-            Smtp.EnableSsl = false;
-            Smtp.UseDefaultCredentials = false;
-            Smtp.Credentials = new System.Net.NetworkCredential(emailCredential.Email, emailCredential.Password);
+            SmtpClient client = new SmtpClient();
+
+            client.Host = GetClientEmail(emailCredential.Email.Substring(emailCredential.Email.LastIndexOf('@') + 1));
+            client.Port = GetClientPost(emailCredential.Email.Substring(emailCredential.Email.LastIndexOf('@') + 1));
+
+            client.UseDefaultCredentials = false;
+            client.EnableSsl = true;
+            client.Credentials = new System.Net.NetworkCredential(emailCredential.Email, emailCredential.Password);
 
             try
             {
-                Smtp.SendAsync(Mail, new { });
-                Mail.Dispose();
+                client.Send(Mail);
 
-                return 1;
+                return result.Successful;
             }
-            catch (Exception ex)
+            catch
             {
-                ex.Message.ToString();
-
-                return 4;
+                return result.ErrorSendingMessage;
             }
-
         }
 
         /// <summary>
@@ -77,7 +89,7 @@ namespace EmailMessaging
         /// <param name="emailCredential">credenciales del Email propietario.</param>
         /// <param name="To">Email de destino.</param>
         /// <param name="Subject">Asulto del mesaje.</param>
-        /// <param name="MessageBsody">Cuerpo del mensaje.</param>
+        /// <param name="MessageBody">Cuerpo del mensaje.</param>
         /// <param name="clientEmail">Cliente email</param>
         /// <param name="AttachDocuments">Documnetos adjuntos.</param>
         /// <returns>Retorn 
@@ -87,60 +99,72 @@ namespace EmailMessaging
         /// 4: Si ubo un error al embiar el mensaje.
         /// </returns>
         /// 
-        public static int SendBulkEmail
-        (EmailCredential emailCredential, string[] To, string Subject, string MessageBsody, ICollection<AttachDocuments> AttachDocuments = null)
+        public static result SendBulkEmail
+        (
+            EmailCredential emailCredential,
+            string[] To,
+            string Subject,
+            string MessageBody,
+            ICollection<Attachment> AttachmentsDocuments = null
+        )
         {
             foreach (string to in To)
             {
                 if (!VerifyEmail(to))
-                    return 2; //Email de destino incorrecto.
+                    return result.WrongDestinationEmail; //Email de destino incorrecto.
             }
 
             if (!VerifyEmail(emailCredential.Email))
-                return 3; //Email incorrecto.
+                return result.WrongEmail; //Email incorrecto.
 
             MailMessage Mail = new MailMessage();
+
+            Parallel.ForEach(To, to =>
+            {
+                Mail.To.Add(new MailAddress(to));
+            });
+
+
             Mail.From = new MailAddress(emailCredential.Email);
+
             Mail.Subject = Subject;
-            Mail.Body = MessageBsody;
+            Mail.SubjectEncoding = Encoding.UTF8;
+
+
+            Mail.Body = MessageBody;
+            Mail.BodyEncoding = Encoding.UTF8;
+
             Mail.IsBodyHtml = true;
             Mail.Priority = MailPriority.High;
 
-            if (AttachDocuments.Equals(null))
-                Parallel.ForEach(AttachDocuments, Document =>
+            if (AttachmentsDocuments != null)
+                Parallel.ForEach(AttachmentsDocuments, Attachments =>
                 {
-                    Mail.Attachments.Add(new Attachment(Document.Document, Document.Name + Document.Extension));
+                    Mail.Attachments.Add(Attachments);
                 });
 
-            SmtpClient Smtp = new SmtpClient();
-            Smtp.Host = GetClientEmail(emailCredential.Email);
-            Smtp.Port = GetClientPost(emailCredential.Email);
-            Smtp.EnableSsl = false;
-            Smtp.UseDefaultCredentials = false;
-            Smtp.Credentials = new System.Net.NetworkCredential(emailCredential.Email, emailCredential.Password);
+            SmtpClient client = new SmtpClient();
+
+            client.Host = GetClientEmail(emailCredential.Email.Substring(emailCredential.Email.LastIndexOf('@') + 1));
+            client.Port = GetClientPost(emailCredential.Email.Substring(emailCredential.Email.LastIndexOf('@') + 1));
+
+            client.UseDefaultCredentials = false;
+            client.EnableSsl = true;
+            client.Credentials = new System.Net.NetworkCredential(emailCredential.Email, emailCredential.Password);
 
             try
             {
-                foreach (var to in To)
-                {
-                    Mail.To.Add(new MailAddress(to));
+                client.Send(Mail);
 
-                    Smtp.SendAsync(Mail, new { });
-                    Mail.Dispose();
-                }
-
-                Smtp.SendAsync(Mail, new { });
-                Mail.Dispose();
-
-                return 1;
+                return result.Successful;
             }
-            catch (Exception ex)
+            catch
             {
-                ex.Message.ToString();
-
-                return 4;
+                return result.ErrorSendingMessage;
             }
         }
+
+
 
         /// <summary>
         /// Metodo asincorono para enviar un mensaje a un correo determinado.
@@ -148,7 +172,7 @@ namespace EmailMessaging
         /// <param name="emailCredential">credenciales del Email propietario.</param>
         /// <param name="To">Email de destino.</param>
         /// <param name="Subject">Asulto del mesaje.</param>
-        /// <param name="MessageBsody">Cuerpo del mensaje.</param>
+        /// <param name="MessageBody">Cuerpo del mensaje.</param>
         /// <param name="clientEmail">Cliente email</param>
         /// <param name="AttachDocuments">Documnetos adjuntos.</param>
         /// <returns>Retorn 
@@ -158,54 +182,65 @@ namespace EmailMessaging
         /// 4: Si ubo un error al embiar el mensaje.
         /// </returns>
         /// 
-        public static async Task<int> SendMailAsync
-        (EmailCredential emailCredential, string To, string Subject, string MessageBsody, ICollection<AttachDocuments> AttachDocuments = null)
+        public static async Task<result> SendMailAsync
+        (
+            EmailCredential emailCredential,
+            string To,
+            string Subject,
+            string MessageBody,
+            ICollection<Attachment> AttachmentsDocuments = null
+        )
         {
             if (!VerifyEmail(To))
-                return 2; //Email de destino incorrecto.
+                return result.WrongDestinationEmail; //Email de destino incorrecto.
 
             if (!VerifyEmail(emailCredential.Email))
-                return 3; //Email incorrecto.
+                return result.WrongEmail; //Email incorrecto.
 
             MailMessage Mail = new MailMessage();
+
             Mail.To.Add(new MailAddress(To));
             Mail.From = new MailAddress(emailCredential.Email);
+
             Mail.Subject = Subject;
-            Mail.Body = MessageBsody;
+            Mail.SubjectEncoding = Encoding.UTF8;
+
+
+            Mail.Body = MessageBody;
+            Mail.BodyEncoding = Encoding.UTF8;
+
             Mail.IsBodyHtml = true;
             Mail.Priority = MailPriority.High;
 
-            if (AttachDocuments.Equals(null))
-                Parallel.ForEach(AttachDocuments, Document =>
+            if (AttachmentsDocuments != null)
+                Parallel.ForEach(AttachmentsDocuments, Attachments =>
                 {
-                    Mail.Attachments.Add(new Attachment(Document.Document, Document.Name + Document.Extension));
+                    Mail.Attachments.Add(Attachments);
                 });
 
-            SmtpClient Smtp = new SmtpClient();
-            Smtp.Host = GetClientEmail(emailCredential.Email);
-            Smtp.Port = GetClientPost(emailCredential.Email);
-            Smtp.EnableSsl = false;
-            Smtp.UseDefaultCredentials = false;
-            Smtp.Credentials = new System.Net.NetworkCredential(emailCredential.Email, emailCredential.Password);
+            SmtpClient client = new SmtpClient();
+
+            client.Host = GetClientEmail(emailCredential.Email.Substring(emailCredential.Email.LastIndexOf('@') + 1));
+            client.Port = GetClientPost(emailCredential.Email.Substring(emailCredential.Email.LastIndexOf('@') + 1));
+
+            client.UseDefaultCredentials = false;
+            client.EnableSsl = true;
+            client.Credentials = new System.Net.NetworkCredential(emailCredential.Email, emailCredential.Password);
 
             try
             {
-                return await Task.Run(() =>
-                {
-                    Smtp.SendAsync(Mail, new { });
-                    Mail.Dispose();
+                client.SendMailAsync(Mail);
 
-                    return 1;
-                });
+                return result.Successful;
             }
-            catch (Exception ex)
+            catch
             {
-                ex.Message.ToString();
-
-                return 4;
+                return result.ErrorSendingMessage;
             }
 
         }
+
+
 
         /// <summary>
         /// Metodo para enviar un mensaje a varios correos electronicos.
@@ -213,7 +248,7 @@ namespace EmailMessaging
         /// <param name="emailCredential">credenciales del Email propietario.</param>
         /// <param name="To">Email de destino.</param>
         /// <param name="Subject">Asulto del mesaje.</param>
-        /// <param name="MessageBsody">Cuerpo del mensaje.</param>
+        /// <param name="MessageBody">Cuerpo del mensaje.</param>
         /// <param name="clientEmail">Cliente email</param>
         /// <param name="AttachDocuments">Documnetos adjuntos.</param>
         /// <returns>Retorn 
@@ -223,85 +258,132 @@ namespace EmailMessaging
         /// 4: Si ubo un error al embiar el mensaje.
         /// </returns>
         /// 
-        public static async Task<int> SendBulkEmailAsync
-        (EmailCredential emailCredential, string[] To, string Subject, string MessageBsody, ICollection<AttachDocuments> AttachDocuments = null)
+        public static async Task<result> SendBulkEmailAsync
+        (
+            EmailCredential emailCredential,
+            string[] To,
+            string Subject,
+            string MessageBody,
+            ICollection<Attachment> AttachmentsDocuments = null
+        )
         {
-            foreach(string to in To)
+            foreach (string to in To)
             {
                 if (!VerifyEmail(to))
-                    return 2; //Email de destino incorrecto.
+                    return result.WrongDestinationEmail; //Email de destino incorrecto.
             }
-             
+
+
             if (!VerifyEmail(emailCredential.Email))
-                return 3; //Email incorrecto.
+                return result.WrongEmail; //Email incorrecto.
 
             MailMessage Mail = new MailMessage();
+
+            Parallel.ForEach(To, to =>
+            {
+                Mail.To.Add(new MailAddress(to));
+            });
+
             Mail.From = new MailAddress(emailCredential.Email);
+
             Mail.Subject = Subject;
-            Mail.Body = MessageBsody;
+            Mail.SubjectEncoding = Encoding.UTF8;
+
+
+            Mail.Body = MessageBody;
+            Mail.BodyEncoding = Encoding.UTF8;
+
             Mail.IsBodyHtml = true;
             Mail.Priority = MailPriority.High;
 
-            if (AttachDocuments.Equals(null))
-                Parallel.ForEach(AttachDocuments, Document =>
+            if (AttachmentsDocuments != null)
+                Parallel.ForEach(AttachmentsDocuments, Attachments =>
                 {
-                    Mail.Attachments.Add(new Attachment(Document.Document, Document.Name + Document.Extension));
+                    Mail.Attachments.Add(Attachments);
                 });
 
-            SmtpClient Smtp = new SmtpClient();
-            Smtp.Host = GetClientEmail(emailCredential.Email);
-            Smtp.Port = GetClientPost(emailCredential.Email);
-            Smtp.EnableSsl = false;
-            Smtp.UseDefaultCredentials = false;
-            Smtp.Credentials = new System.Net.NetworkCredential(emailCredential.Email, emailCredential.Password);
+            SmtpClient client = new SmtpClient();
+
+            client.Host = GetClientEmail(emailCredential.Email.Substring(emailCredential.Email.LastIndexOf('@') + 1));
+            client.Port = GetClientPost(emailCredential.Email.Substring(emailCredential.Email.LastIndexOf('@') + 1));
+
+            client.UseDefaultCredentials = false;
+            client.EnableSsl = true;
+            client.Credentials = new System.Net.NetworkCredential(emailCredential.Email, emailCredential.Password);
 
             try
             {
-                return await Task.Run(() =>
-                {
-                    foreach (var to in To)
-                    {
-                        Mail.To.Add(new MailAddress(to));
+                client.SendMailAsync(Mail);
 
-                        Smtp.SendAsync(Mail, new { });
-                        Mail.Dispose();
-                    }
-
-                    return 1;
-                });
+                return result.Successful;
             }
-            catch (Exception ex)
+            catch
             {
-                ex.Message.ToString();
-
-                return 4;
+                return result.ErrorSendingMessage;
             }
         }
 
+
+
+
         private static string GetClientEmail(string clientEmail) =>
-            clientEmail.ToLower()[(clientEmail.IndexOf("@") + 1)..] switch
+            clientEmail switch
             {
                 "live.com" => "smtp.live.com",
-                "hotmail.com" => "smtp.hotmail.com",
-                "Gmail.com" => "smtp.gmail.com",
-                "Yahoo.com" => "smtp.mail.yahoo.com",
-                "Outlook.com" => "Smtp.live.com",
+                "hotmail.com" => "smtp.live.com",
+                "gmail.com" => "smtp-relay.gmail.com",
+                "yahoo.com" => "smtp.mail.yahoo.com",
+                "outlook.com" => "Smtp.live.com",
                 _ => throw new NotImplementedException()
             };
 
+
+
         private static int GetClientPost(string clientEmail) =>
-            clientEmail.ToLower()[(clientEmail.IndexOf("@") + 1)..] switch
+            clientEmail switch
             {
                 "live.com" => 587,
                 "hotmail.com" => 587,
-                "Gmail.com" => 587,
-                "Yahoo.com" => 465,
-                "Outlook.com" => 587,
+                "gmail.com" => 587,
+                "yahoo.com" => 465,
+                "outlook.com" => 587,
+                _ => throw new NotImplementedException()
+            };
+
+
+
+        public enum result
+        {
+            Successful,
+            WrongDestinationEmail,
+            WrongEmail,
+            ErrorSendingMessage
+        }
+
+
+        public static string GetResulInesDO(result result) =>
+            result switch
+            {
+                result.Successful => "Exitoso",
+                result.WrongDestinationEmail => "Email de destino incorrecto",
+                result.WrongEmail => "Email incorrecto",
+                result.ErrorSendingMessage => "Error al enviar el mensaje puede ser error de coneccion",
+                _ => throw new NotImplementedException()
+            };
+
+
+        public static string GetResulIninUS(result result) =>
+            result switch
+            {
+                result.Successful => "Successful",
+                result.WrongDestinationEmail => "Wrong destination email",
+                result.WrongEmail => "Wrong email",
+                result.ErrorSendingMessage => "Error when sending the message may be a connection error",
                 _ => throw new NotImplementedException()
             };
 
         private static bool VerifyEmail(String email) =>
-            Regex.IsMatch(email, "\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*") && 
+            Regex.IsMatch(email, "\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*") &&
             (Regex.Replace(email, "\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*", String.Empty).Length == 0);
 
     }
